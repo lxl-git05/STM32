2074ecf8c6fec2c3cc1bec4f84ed832357d06b7d
lxl-git05 <XinLiu051024@outlook.com>
2026-04-18 10:55:19 +0800

    完成mode3测试模式构建

--- Projects/Robot2026/Template/Mode/Mode_3.c
+++ Projects/Robot2026/Template/Mode/Mode_3.c
@@ -11,39 +11,10 @@ void Mode_3_Setup(void)
 
 void Mode_3_Loop(void)
 {
-	// // 1. 测试串口功能
-    // if (Serial_GetNewPackageFlag_ABC(&Serial1))
-    // {
-    //     Serial_SetFloatData(&Serial1, "Kp", "Kp=%f", &check) ;
-    //     Serial_printf(&Serial1 , "%f\n", &check) ;
-    // }
-    // OLED_Printf(0, 40, OLED_6X8, "%.2f" , check) ;  // 测试OLED
+	// 本loop函数建议只执行一个check任务,防止未知Bug
+    
     // 2. 测试电机
-//     OLED_ClearArea(0, 10, 120, 10) ;
-//     OLED_Printf(0 , 10, OLED_6X8, "A:%.0f" ,  Motor_A.PID_s.realPoint_Now) ;
-//     OLED_Printf(60, 10, OLED_6X8, "B:%.0f" , -Motor_B.PID_s.realPoint_Now) ;
-//     // Serial参数更改
-//     if (Serial_GetNewPackageFlag_ABC(&Serial1))
-//     {
-// #ifdef  Motoe_A_PID_Check
-//         // 得到数据
-//         Serial_SetFloatData(&Serial1, "Kp", "Kp=%f", &Motor_A.PID_s.Kp) ;
-//         Serial_SetFloatData(&Serial1, "Ki", "Ki=%f", &Motor_A.PID_s.Ki) ;
-//         Serial_SetFloatData(&Serial1, "Kd", "Kd=%f", &Motor_A.PID_s.Kd) ;
-//         Serial_SetFloatData(&Serial1, "goalPoint_A", "goalPoint_A=%f", &Motor_A.PID_s.goalPoint) ;
-//         Motor_SetSpeed(&Motor_A, Motor_A.PID_s.goalPoint) ;
-//         // OLED展示PID参数
-//         OLED_Printf(0, 30, OLED_6X8, "%.2f,%.2f,%.2f" , Motor_A.PID_s.Kp , Motor_A.PID_s.Ki , Motor_A.PID_s.Kd) ;
-// #else 
-//         Serial_SetFloatData(&Serial1, "Kp", "Kp=%f", &Motor_B.PID_s.Kp) ;
-//         Serial_SetFloatData(&Serial1, "Ki", "Ki=%f", &Motor_B.PID_s.Ki) ;
-//         Serial_SetFloatData(&Serial1, "Kd", "Kd=%f", &Motor_B.PID_s.Kd) ;
-//         Serial_SetFloatData(&Serial1, "goalPoint_B", "goalPoint_B=%f", &Motor_B.PID_s.goalPoint) ;
-//         Motor_SetSpeed(&Motor_B, Motor_B.PID_s.goalPoint) ;
-//         // OLED展示PID参数
-//         OLED_Printf(0, 30, OLED_6X8, "%.2f,%.2f,%.2f" , Motor_B.PID_s.Kp , Motor_B.PID_s.Ki , Motor_B.PID_s.Kd) ;
-// #endif  
-//      }
+    
 }
 
 void Mode_3_Exit(void)
@@ -54,4 +25,15 @@ void Mode_3_Exit(void)
 void Mode_3_Tick(void)
 {
 	
+}
+
+// 1. 测试串口功能
+void Check_Serial(Serial_Typedef* pSerial)
+{
+    if (Serial_GetNewPackageFlag_ABC(pSerial))
+    {
+        Serial_SetFloatData(pSerial, "Kp", "Kp=%f", &check) ;
+        Serial_printf(pSerial , "%f\n", &check) ;
+    }
+    OLED_Printf(0, 40, OLED_6X8, "%.2f" , check) ;  // 测试OLED
 }
