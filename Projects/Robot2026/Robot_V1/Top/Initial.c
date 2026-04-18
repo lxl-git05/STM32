8a01fffa16339f3a33939674aebc52d9fcf05a21
lxl-git05 <XinLiu051024@outlook.com>
2026-04-18 10:57:55 +0800

    加入正式比赛工程

---
+++ Projects/Robot2026/Robot_V1/Top/Initial.c
@@ -0,0 +1,23 @@
+#include "Initial.h"
+#include "AllHeader.h"
+
+// Mymain初始化集合,使主函数更简洁
+void Initial_ALL(void)
+{
+	// Hardware
+	OLED_Init() ;																	// OLED初始化
+	
+	// Software
+	Serial_Init() ;																// 串口初始化
+	
+	// Tools
+	Flash_Mode_Init() ;														// LED闪烁工具初始化
+	Timer_Counter_Init() ;												// 时间戳测定初始化
+}
+
+// 定时器初始化
+void Initial_Timer(void)
+{
+	Timer_Initial() ;
+}
+
